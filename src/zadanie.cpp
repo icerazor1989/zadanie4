7461e27369d54528ad0e90846230b25f401d5f59
icerazor <razor.89@mail.ru>
2021-05-24 17:27:18 +0300

    First commit

---
+++ src/zadanie.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int a;
+   int b;
+   int sum=0;
+   cout<<"enter two numbers : " <<endl;
+   cin  >>a >> b ;
+
+   sum=a+b;
+
+if ( 10<=sum && sum<=20  )
+{
+    cout<< "true"<< endl;
+}
+else
+{
+    cout<< "false" << endl;
+}
+    return 0;
+}
