cad4ab68bef4d150a4c35cbf4ca93bcba579b79a
icerazor <razor.89@mail.ru>
2021-05-24 18:53:34 +0300

    First commit

---
+++ src/zadanie.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int y;
+    cout<<"Enter the year: "<<endl;
+    cin>> y;
+    if (y%4 != 0 || y%100 == 0 && y%400 != 0)
+    {
+
+        cout << "This is a normal year"<<endl;
+
+    }
+
+    else
+    {
+        cout <<"This is a leap year"<<endl;
+    }
+    return 0;
+}
